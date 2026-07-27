3b542484486784d43e91815adeff525ebb0e3adf
abdulkhan-ic <abdul.khan@infocentric.com.au>
2026-07-27 13:47:25 +1000

    Update streamlit_app.py
    Expose JSON data inside the response object.

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ if time_to_insert:
     st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+# smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json())
 
